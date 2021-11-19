c39ce6407d099f259121e19122a8bb63c44c93fb
Louis <louis.m.newton@hotmail.fr>
2021-11-19 23:05:15 +0100

    serveur node et page html

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+const http = require("http").createServer(app);
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+})
+
+ http.listen(3000, () => {
+     console.log("Port 3000 actif")
+ })
