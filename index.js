7c73a1b4a5133a84696124c2f535b0b24735698b
Louis <louis.m.newton@hotmail.fr>
2021-11-19 23:30:24 +0100

    chat sans sauvegarde

--- index.js
+++ index.js
@@ -3,8 +3,22 @@ const app = express();
 
 const http = require("http").createServer(app);
 
+const io = require("socket.io")(http);
+
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
+})
+
+io.on("connection", (socket) => {
+    console.log("Un user s'est connecté");
+
+    socket.on("disconnect", () =>{
+        console.log("Un user s'est déconnecté");
+    });
+
+    socket.on("chat_message", (msg) => {
+        io.emit("chat_message", msg);
+    })
 })
 
  http.listen(3000, () => {
